d5eaa4a232cb8af0c6dd7ed0a4eec3223448387f
André de Brito <ABritoPT@users.noreply.github.com>
2025-11-23 15:56:05 +0100

    Logger and Yaml loader

---
+++ scripts/windows/win-backups/win_backups.py
@@ -0,0 +1,18 @@
+import logging
+import sys
+import yaml
+
+TEST_JOB='d:/André/coding/git/scripts-and-configs/scripts/windows/win-backups/jobs/main.yaml'
+dryRun = True
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+stream_handler = logging.StreamHandler(stream=sys.stdout)
+# logFormatter = logging.Formatter("%(asctime)s [%(levelname)-5.5s] %(message)s")
+# stream_handler.setFormatter(logFormatter)
+logger.addHandler(stream_handler)
+logger.debug("logger set")
+
+with open(TEST_JOB, 'r') as file:
+    job = yaml.safe_load(file)
+# print(job)
