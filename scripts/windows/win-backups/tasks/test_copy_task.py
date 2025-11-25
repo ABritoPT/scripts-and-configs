476992c475391c4ccb7356633f45835e1e80eb92
André de Brito <ABritoPT@users.noreply.github.com>
2025-11-25 13:10:02 +0100

    Introduced conftest.py central test configs

--- scripts/windows/win-backups/tasks/test_copy_task.py
+++ scripts/windows/win-backups/tasks/test_copy_task.py
@@ -2,71 +2,8 @@ import pytest
 import re
 import subprocess
 from os import path
-import time
+import conftest
 from copy_task import Copy
-
-def get_tests_dir():
-    return path.abspath(path.join(path.dirname(path.realpath(__file__)),
-                                  path.pardir ,"tests"))
-
-@pytest.fixture
-def empty_test_file():
-    test_file_name = "test_script_" + str(time.time_ns())
-    test_file_path = path.join(get_tests_dir(), test_file_name)
-    res = subprocess.call("copy /b NUL " + test_file_path, shell=True)
-    assert res==0, "setup successful"
-    
-    yield test_file_path
-
-    res = subprocess.call("del " + test_file_path, shell=True)
-    assert path.exists(test_file_path) == False, "teardown successful"
-
-def cars_directory_contents():
-    return {
-        'subaru': {
-            '?files': [ 'BRZ.txt', 'WRX.txt', 'Impreza.txt' ]
-        },
-        'peugeot': {
-            '205': {
-                '?files': [ '205-gti.txt', '205-xs.txt' ]
-            },
-            '106': {
-                '?files': [ '106-xs.txt', '106-grd.txt' ]
-            },
-            '?files': ['RCZ.txt']
-        }
-    }
-
-def __process_dir(dir_name, dir_contents, parent_dir):
-    curr_dir = path.join(parent_dir, dir_name)
-    outcome = subprocess.call("md " + curr_dir, shell=True)
-
-    for k,v in dir_contents.items():
-        if k == '?files':
-            for f in v:
-                f_path = path.join(curr_dir, f)
-                res = subprocess.call("copy /b NUL " + f_path, shell=True)
-                outcome = max(res,outcome)
-        else:
-            res = __process_dir(k, v, curr_dir)
-            outcome = max(res,outcome)
-    return outcome
-
-def __recurse_delete_dir(path):
-    subprocess.call("rd /s /q " + path, shell=True)
-
-@pytest.fixture
-def cars_directory_structure():
-    contents = cars_directory_contents()
-    tests_dir = get_tests_dir()
-    car_tests_dir = path.join(tests_dir, "cars_test")
-    res = __process_dir("cars_test", contents, tests_dir)
-    assert res==0, "setup successful"
-
-    yield car_tests_dir
-
-    res = __recurse_delete_dir(car_tests_dir)
-    assert path.exists(car_tests_dir) == False, "teardown successful"
 
 def test_fake_source():
     dir = "ZZZ:\\fake\\folder"
@@ -98,4 +35,4 @@ def test_complex_copy(cars_directory_structure):
     assert path.exists(path.join(dst, "peugeot", "106", "106-grd.txt")) == True
     assert path.exists(path.join(dst, "peugeot", "RCZ.txt")) == True
     
-    __recurse_delete_dir(dst)
+    conftest.recurse_delete_dir(dst)
--- scripts/windows/win-backups/tasks/test_script_task.py
+++ scripts/windows/win-backups/tasks/test_script_task.py
@@ -1,26 +1,11 @@
 import pytest
-import subprocess
 from os import path
-import time
 from script_task import Script
 
 def test_fake_folder():
     sc = Script("fake folder test", "dir", "ZZZ:\\fake\\folder")
     with pytest.raises(ValueError, match="Start in folder does not exist"):
         sc.run()
-
-@pytest.fixture
-def empty_test_file():
-    test_file_name = "test_script_" + str(time.time_ns())
-    test_file_path = path.abspath(path.join(path.dirname(path.realpath(__file__)),
-                                  path.pardir ,"tests", test_file_name))
-    res = subprocess.call("copy /b NUL " + test_file_path, shell=True)
-    assert res==0, "setup successful"
-    
-    yield test_file_path
-
-    res = subprocess.call("del "+test_file_path, shell=True)
-    assert path.exists(test_file_path) == False, "teardown successful"
 
 def test_dry_run(empty_test_file):
     sc = Script("dry-run test delete file", "del "+empty_test_file)
