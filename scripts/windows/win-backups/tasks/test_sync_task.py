cf5ac280e498426b7537ab1dc36c518ee58e74fa
André de Brito <ABritoPT@users.noreply.github.com>
2025-11-29 14:23:29 +0100

    Minor refactoring

--- scripts/windows/win-backups/tasks/test_sync_task.py
+++ scripts/windows/win-backups/tasks/test_sync_task.py
@@ -7,29 +7,29 @@ from sync_task import SyncTask
 
 def test_fake_source():
     dir = "ZZZ:\\fake\\folder"
-    ct = SyncTask("fake source test", dir, dir)
+    st = SyncTask("fake source test", dir, dir)
     with pytest.raises(ValueError, match=re.escape("Path does not exist: " + dir)):
-        ct.run()
+        st.run()
 
 def test_dry_run(empty_test_file):
     dst = empty_test_file + ".cpy"
-    ct = SyncTask("dry-run test sync file", empty_test_file, dst)
-    ct.run(True)
+    st = SyncTask("dry-run test sync file", empty_test_file, dst)
+    st.run(True)
     assert path.exists(dst) == False
 
 def test_real_run(empty_test_file):
     dst = empty_test_file + ".cpy"
-    ct = SyncTask("dry-run test sync file", empty_test_file, dst)
-    ct.run(False)
+    st = SyncTask("dry-run test sync file", empty_test_file, dst)
+    st.run(False)
     assert path.exists(dst) == True
     subprocess.call("del " + dst, shell=True)
 
 def test_complex_sync(cars_directory_structure):
     src = cars_directory_structure
     dst = src + "-copy"
-    ct = SyncTask("complex sync test", src, dst)
+    st = SyncTask("complex sync test", src, dst)
     
-    ct.run(False)
+    st.run(False)
     assert path.exists(path.join(dst, "subaru", "BRZ.txt")) == True
     assert path.exists(path.join(dst, "peugeot", "205", "205-xs.txt")) == True
     assert path.exists(path.join(dst, "peugeot", "106", "106-grd.txt")) == True
