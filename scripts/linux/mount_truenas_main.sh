2a0968b73497fddc5bdc43d3f45f18bcdc041e49
ADB <andre.manuel.debrito@gmail.com>
2026-02-24 20:37:52 +0100

    Update mount path to S24's

--- scripts/linux/mount_truenas_main.sh
+++ scripts/linux/mount_truenas_main.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mount -t cifs -o rw,vers=3.0,credentials=/root/.smbcredentials //192.168.1.200/main /mnt/truenas_main
+mount -t cifs -o rw,vers=3.0,credentials=/root/.smbcredentials //192.168.1.200/main_S24 /mnt/truenas_main
